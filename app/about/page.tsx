138440058b06f739191ba4356f54d26475418aca
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-19 14:49:54 +0600

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>THis is about page</h1>
+    </div>
+  );
+}
---
+++ app/blogs/page.tsx
@@ -0,0 +1,7 @@
+export default function Blogs() {
+      return (
+            <div>
+                  <h1>this is blogs page</h1>
+            </div>
+      );
+}

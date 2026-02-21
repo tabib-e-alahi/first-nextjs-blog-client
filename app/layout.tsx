c60be69f5826922e34990e0b2dba8d6b055947c5
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-21 19:18:09 +0600

    Remove Navbar from RootLayout and add to CommonLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/layout/Navbar";
 import { ThemeProvider } from "./providers/ThemeProvider";
 
 const geistSans = Geist({
@@ -34,7 +33,6 @@ export default function RootLayout({
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange>
-          <Navbar></Navbar>
           {children}
         </ThemeProvider>
       </body>

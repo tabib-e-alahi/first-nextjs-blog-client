43e60a8df607743e0b4a08033d6f94d518414fe7
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-21 11:53:27 +0600

    Remove 'use client' from about pages

--- app/about/error.tsx
+++ app/about/error.tsx
@@ -1,3 +1,4 @@
+"use client"
 export default function error() {
   return (
     <div>
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,10 @@
 export default async function AboutPage() {
+  // for testing the loading.tsx is working orn not
   await new Promise((resolve) => {
     setTimeout(resolve, 4000);
   })
+
+  
   return (
     <div>
       <h1>THis is about page</h1>

ff2271ef9b9f42068167b929bf94729f8c2739cf
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-22 14:18:38 +0600

    Update WriteBlogPage title with styling

--- app/(dashboardLayout)/dashboard/write-blog/page.tsx
+++ app/(dashboardLayout)/dashboard/write-blog/page.tsx
@@ -1,7 +1,7 @@
 export default function WriteBlogPage() {
   return (
     <div>
-      <h1>this si write blog page</h1>
+      <h1 className="text-center text-6xl mt-20">this is <span className="text-red-500">write blog</span> page</h1>
     </div>
   );
 }

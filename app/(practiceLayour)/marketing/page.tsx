8741d3acd0a739c866ccda541146705d48c75d66
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-22 15:35:26 +0600

    Update page titles in development and marketing pages

--- app/(practiceLayour)/marketing/page.tsx
+++ app/(practiceLayour)/marketing/page.tsx
@@ -1,7 +1,7 @@
 export default function DevelopmentPage() {
   return (
     <div className="size-200 border-2 border-blue-500">
-      <h1 className="text-2xl"> Development </h1>
+      <h1 className="text-4xl text-center mt-5"> Marketing </h1>
     </div>
   );
 }

0a8169979c38cc67da878db2a568b30fb6a46839
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-22 15:33:27 +0600

    Add Development page and link to layout

--- app/(practiceLayour)/layout.tsx
+++ app/(practiceLayour)/layout.tsx
@@ -4,6 +4,7 @@ export default function RootLayout() {
   return (
     <div>
       <nav className="flex gap-10 m-8">
+            <Link className="hover:underline" href="/development">Development</Link>
             <Link className="hover:underline" href="/marketing">Marketing</Link>
             <Link className="hover:underline" href="/marketing/settings">Settings</Link>
             <Link className="hover:underline" href="/sales">Sales</Link>

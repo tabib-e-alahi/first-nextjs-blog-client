00c813aea7beba20e96493fd19ab7d598ff18565
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-22 15:39:27 +0600

    Rename marketing and sales pages to use slot-based routing

--- app/(practiceLayour)/layout.tsx
+++ app/(practiceLayour)/layout.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
 
-export default function RootLayout({children}:{children: React.ReactNode}) {
-  return (
-    <div>
-      <nav className="flex gap-10 m-8">
-            <Link className="hover:underline" href="/development">Development</Link>
-            <Link className="hover:underline" href="/marketing">Marketing</Link>
-            <Link className="hover:underline" href="/marketing/settings">Settings</Link>
-            <Link className="hover:underline" href="/sales">Sales</Link>
-            <Link className="hover:underline" href="/testing">Testing</Link>
-      </nav>
-      {children}
-    </div>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+      return (
+            <div>
+                  <nav className="flex gap-10 m-8">
+                        <Link className="hover:underline" href="/development">Development</Link>
+                        <Link className="hover:underline" href="/marketing">Marketing</Link>
+                        <Link className="hover:underline" href="/marketing/settings">Settings</Link>
+                        <Link className="hover:underline" href="/sales">Sales</Link>
+                        <Link className="hover:underline" href="/testing">Testing</Link>
+                  </nav>
+                  {children}
+            </div>
+      );
 }

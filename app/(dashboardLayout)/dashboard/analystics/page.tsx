b156dd04ff8e96d1eca200353802db6f73b71cf8
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-22 14:17:39 +0600

    Update dashboard layout and sidebar links

--- app/(dashboardLayout)/dashboard/analystics/page.tsx
+++ app/(dashboardLayout)/dashboard/analystics/page.tsx
@@ -1,7 +1,7 @@
 export default function AnalysticsPage() {
   return (
     <div>
-      <h1>thsi is Analystics page</h1>
+      <h1 className="text-center text-6xl mt-20">this is <span className="text-red-500">Analystics</span> page</h1>
     </div>
   );
 }
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -29,7 +29,7 @@ const data = {
         },
         {
           title: "Dashboard",
-          url: "/",
+          url: "/dashboard",
         },
         {
           title: "Write Blog",

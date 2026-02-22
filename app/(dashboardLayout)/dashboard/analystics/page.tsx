5b09d14c051fa15e3e2cd3b29bab21ee8f494c02
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-22 14:15:38 +0600

    Add dashboard pages: Analystics and Write Blog

---
+++ app/(dashboardLayout)/dashboard/analystics/page.tsx
@@ -0,0 +1,7 @@
+export default function AnalysticsPage() {
+  return (
+    <div>
+      <h1>thsi is Analystics page</h1>
+    </div>
+  );
+}
---
+++ app/(dashboardLayout)/dashboard/write-blog/page.tsx
@@ -0,0 +1,7 @@
+export default function WriteBlogPage() {
+  return (
+    <div>
+      <h1>page</h1>
+    </div>
+  );
+}

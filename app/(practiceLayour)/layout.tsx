cc35c4b32a2ac9ddb7919dcaa01d89bacd22feba
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-22 15:31:26 +0600

    Update layout with improved navigation styling

--- app/(practiceLayour)/layout.tsx
+++ app/(practiceLayour)/layout.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function RootLayout() {
   return (
     <div>
-      <nav>
-            <Link  href="">Marketing</Link>
-            <Link href="">Settings</Link>
-            <Link href="">Sales</Link>
-            <Link href="">Testing</Link>
+      <nav className="flex gap-10 m-8">
+            <Link className="hover:underline" href="/marketing">Marketing</Link>
+            <Link className="hover:underline" href="/marketing">Settings</Link>
+            <Link className="hover:underline" href="">Sales</Link>
+            <Link className="hover:underline" href="">Testing</Link>
       </nav>
     </div>
   );

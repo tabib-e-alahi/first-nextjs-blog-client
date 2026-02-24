087358a2f643d4f08644acf51fecc901480ae26f
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-24 19:45:57 +0600

    Update API fetch to use 'no-store' cache and include Cookie header

--- app/(commonLayout)/page.tsx
+++ app/(commonLayout)/page.tsx
@@ -4,18 +4,19 @@ import { cookies } from "next/headers";
 export default async function Home() {
   const cookieStore = await cookies();
 
-  const res= await fetch("http://localhost:8000/api/auth/get-session",{
-    headers:{
+  const res = await fetch("http://localhost:8000/api/auth/get-session", {
+    headers: {
       Cookie: cookieStore.toString()
-    }
+    },
+    cache: "no-store"
   })
-  
+
   const session = await res.json();
 
   console.log(session);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     <h1>This is app page</h1>
+      <h1>This is app page</h1>
     </div>
   );
 }

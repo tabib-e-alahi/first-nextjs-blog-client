d65fe1d07fd7e50eb859ca468ef04d72db20235b
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-24 12:19:29 +0600

    \`Update Home page to use async function\`

--- app/(commonLayout)/page.tsx
+++ app/(commonLayout)/page.tsx
@@ -1,5 +1,6 @@
 
-export default function Home() {
+export default async function Home() {
+  con st 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
      <h1>This is app page</h1>

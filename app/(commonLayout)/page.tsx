4b9633c426558526af00b95f97224102321b5e14
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-24 12:20:28 +0600

    Add auth client session retrieval to Home page

--- app/(commonLayout)/page.tsx
+++ app/(commonLayout)/page.tsx
@@ -1,6 +1,8 @@
+import { authClient } from "@/lib/auth-client";
 
 export default async function Home() {
-  con st 
+  const session = await authClient.getSession();
+  console.log(session);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
      <h1>This is app page</h1>

a2d2cba54e201a6ed1fc63eb62cc11184f7b63d6
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-24 11:56:32 +0600

    Update register form component

--- app/(commonLayout)/register/page.tsx
+++ app/(commonLayout)/register/page.tsx
@@ -1,10 +1,10 @@
-import { SignupForm } from "@/components/register-form"
+import { RegsiterForm } from "@/components/register-form"
 
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <SignupForm />
+        <RegsiterForm />
       </div>
     </div>
   )

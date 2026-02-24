15bb0d3c61795691d8fc2c2c6d6e9de2176c5308
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-24 11:20:27 +0600

    Rename practiceLayout directory and add marketing and sales slots

---
+++ app/(practiceLayout)/@salesSlot/default.tsx
@@ -0,0 +1,7 @@
+export default function SalesDefault() {
+  return (
+    <div className="size-200 border-2 border-red-500">
+      <h1 className="text-2xl"> This is a placeholder for sales </h1>
+    </div>
+  );
+}

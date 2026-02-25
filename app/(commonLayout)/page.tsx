34555efbe2a5623031d2e073e65db878d2fbc916
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-25 15:01:14 +0600

    Add logging for user session data

--- app/(commonLayout)/page.tsx
+++ app/(commonLayout)/page.tsx
@@ -3,6 +3,7 @@ import { userServie } from "@/services/user.service";
 
 export default async function Home() {
   const {data} = await userServie.getSession();
+  console.log(data);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>This is app page</h1>

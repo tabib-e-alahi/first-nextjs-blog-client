41a3c915a3df6591f83acfefe157e36f18d8f96a
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-25 15:03:10 +0600

    Add error handling to getSession API call

--- app/(commonLayout)/page.tsx
+++ app/(commonLayout)/page.tsx
@@ -2,8 +2,9 @@ import { userServie } from "@/services/user.service";
 
 
 export default async function Home() {
-  const {data} = await userServie.getSession();
+  const {data, error} = await userServie.getSession();
   console.log(data);
+  console.log(error);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>This is app page</h1>

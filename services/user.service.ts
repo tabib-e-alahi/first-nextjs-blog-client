a2567094d75b52347bbc93452599b2e9b4c2c455
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-25 14:57:12 +0600

    Implement user session retrieval via API

--- services/user.service.ts
+++ services/user.service.ts
@@ -1,5 +1,25 @@
+import { cookies } from "next/headers";
+
 export const userServie = {
-      getSession: async function(){
-            try
-      }
-}
+    getSession: async function () {
+        try {
+            const cookieStore = await cookies();
+
+            const res = await fetch(
+                "http://localhost:8000/api/auth/get-session",
+                {
+                    headers: {
+                        Cookie: cookieStore.toString(),
+                    },
+                    cache: "no-store",
+                },
+            );
+
+            const session = await res.json();
+
+            return session;
+        } catch (error) {
+            console.error(error)
+        }
+    },
+};

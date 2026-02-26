246a9e3f149214c1b7f55bd32920df7690dfcbce
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-26 18:57:43 +0600

    Update env.ts with server URL and runtimeEnv

--- env.ts
+++ env.ts
@@ -1,2 +1,9 @@
 import { createEnv } from "@t3-oss/env-nextjs"; // or core package
 import { z } from "zod";
+
+export const env = createEnv({
+      server:{
+            BACKEND_URL
+      },
+      runtimeEnv
+})

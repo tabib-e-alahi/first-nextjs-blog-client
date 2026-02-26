8275b077bbfef386b741e76d4144a848c4456a57
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-26 19:01:42 +0600

    Replace hardcoded API URLs with environment variables

--- env.ts
+++ env.ts
@@ -8,10 +8,10 @@ export const env = createEnv({
         API_URL: z.url(),
         AUTH_URL: z.url(),
     },
-    runtimeEnv:{
-        BACKEND_API: z.url(),
-        FRONTEND_API: z.url(),
-        API_URL: z.url(),
-        AUTH_URL: z.url(),
-    }
+    runtimeEnv: {
+        BACKEND_API: process.env.BACKEND_API,
+        FRONTEND_API: process.env.FRONTEND_API,
+        API_URL: process.env.API_URL,
+        AUTH_URL: process.env.AUTH_URL,
+    },
 });

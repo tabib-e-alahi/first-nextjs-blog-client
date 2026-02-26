2c72a6f5cb4a87630d09a39833fab27e954f41aa
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-26 23:41:16 +0600

    Rename dashboard pages to reflect user roles

--- app/(dashboardLayout)/dashboard/@admin/default.tsx
+++ app/(dashboardLayout)/dashboard/@admin/default.tsx
@@ -1,7 +1,3 @@
-export default function AnalysticsPage() {
-  return (
-    <div>
-      <h1 className="text-center text-6xl mt-20">this is <span className="text-red-500">Analystics</span> page</h1>
-    </div>
-  );
+export default function AdminDefault() {
+  return null;
 }

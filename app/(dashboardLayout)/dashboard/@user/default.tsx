b9c07f47b9d80959356b936ee5c484b407362c3c
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-26 23:42:16 +0600

    Add user dashboard and default page components

--- app/(dashboardLayout)/dashboard/@user/default.tsx
+++ app/(dashboardLayout)/dashboard/@user/default.tsx
@@ -1,7 +1,3 @@
-export default function WriteBlogPage() {
-  return (
-    <div>
-      <h1 className="text-center text-6xl mt-20">this is <span className="text-red-500">write blog</span> page</h1>
-    </div>
-  );
+export default function UserDefault() {
+  return null;
 }

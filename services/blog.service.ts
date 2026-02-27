005de11097aef7d4551c02197f0e146fb774f831
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-27 12:09:08 +0600

    Remove success check for API response

--- services/blog.service.ts
+++ services/blog.service.ts
@@ -7,9 +7,9 @@ export const blogService = {
             try {
                   const res = await fetch(`${API_URL}/posts`);
                   const data = await res.json()
-                  if(data.sucess){
-                        return {data: null, error:{message:"Data is missing"}}
-                  }
+                  // if(data.sucess){
+                  //       return {data: null, error:{message:"Data is missing"}}
+                  // }
                   return {data: data, error: null}
             } catch (error) {
                   return{data: null, error:{message:"Something went wrong"}}

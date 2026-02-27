1f0ba03433b3529363fc53bfa40ebff747674b49
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-27 12:23:09 +0600

    Update blog service with revalidation

--- services/blog.service.ts
+++ services/blog.service.ts
@@ -1,18 +1,20 @@
-import { env } from "@/env"
+import { env } from "@/env";
 
-const API_URL = env.API_URL
+const API_URL = env.API_URL;
 
 export const blogService = {
-      getBlogPosts: async function () {
-            try {
-                  const res = await fetch(`${API_URL}/posts`, {next:{revalidate: }});
-                  const data = await res.json()
-                  // if(data.sucess){
-                  //       return {data: null, error:{message:"Data is missing"}}
-                  // }
-                  return {data: data, error: null}
-            } catch (error) {
-                  return{data: null, error:{message:"Something went wrong"}}
-            }
-      }
-}
+    getBlogPosts: async function () {
+        try {
+            const res = await fetch(`${API_URL}/posts`, {
+                next: { revalidate: 10 },
+            });
+            const data = await res.json();
+            // if(data.sucess){
+            //       return {data: null, error:{message:"Data is missing"}}
+            // }
+            return { data: data, error: null };
+        } catch (error) {
+            return { data: null, error: { message: "Something went wrong" } };
+        }
+    },
+};

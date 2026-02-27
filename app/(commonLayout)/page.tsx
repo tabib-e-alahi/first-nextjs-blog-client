ee6ced5f19c1743939aa080d0494a42d969fc769
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-27 11:57:10 +0600

    Remove unused user service logging

--- app/(commonLayout)/page.tsx
+++ app/(commonLayout)/page.tsx
@@ -2,9 +2,7 @@ import { userServie } from "@/services/user.service";
 
 
 export default async function Home() {
-  const {data, error} = await userServie.getSession();
-  console.log(data);
-  console.log(error);
+  const {data} = aw
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>This is app page</h1>

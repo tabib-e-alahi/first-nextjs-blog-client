65b2ba32813e7345b3d55edc3a96df8862b9bb55
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-27 11:58:08 +0600

    Add blog service and fetch blog posts

--- app/(commonLayout)/page.tsx
+++ app/(commonLayout)/page.tsx
@@ -1,8 +1,10 @@
+import { blogService } from "@/services/blog.service";
 import { userServie } from "@/services/user.service";
 
 
 export default async function Home() {
-  const {data} = aw
+  const {data} = await blogService.getBlogPosts();
+  console.log(data);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>This is app page</h1>

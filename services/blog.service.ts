1dc57294abad08d272d878cce08463828906d8ca
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-27 11:54:08 +0600

    Fix blog service to return error on missing data

--- services/blog.service.ts
+++ services/blog.service.ts
@@ -8,9 +8,9 @@ export const blogService = {
                   const res = await fetch(`${API_URL}/posts`);
                   const data = await res.json()
                   if(data.sucess){
-
+                        return {data: null, error:{message:"Data is missing"}}
                   }
-                  
+                  return {data: data, error: null}
             } catch (error) {
                   return{data: null, error:{message:"Something went wrong"}}
             }
